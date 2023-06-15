f61dc40b455fd16fffd33089b9a46cc70e684ede
Daniel Quero <danielquero95@gmail.com>
2023-06-15 13:37:34 +0200

    Move all files to ui folder (following clean architecture principles))

---
+++ src/ui/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+	<main>
+		<TheWelcome />
+	</main>
+</template>
+
+<script lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+	components: {
+		TheWelcome,
+	},
+})
+export default class HomeView extends Vue {}
+</script>
