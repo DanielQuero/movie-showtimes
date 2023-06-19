e55e03f07f5ad4e505941c1dcf9e7470feb90a42
Daniel Quero <danielquero95@gmail.com>
2023-06-19 19:13:07 +0200

    Created movieCard component and redo home view

---
+++ src/ui/views/home/components/movieCard/movieCard.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+.movie-box
+  .movie-img-box
+    img.movie-img(:src="movieInfo.posterImgUrl")
+  .movie-info-box
+    .rating-row
+      StarRating(:rating="movieInfo.voteAverage")
+      b.vote-average {{ movieInfo.voteAverage }}
+    .movie-title {{ movieInfo.title }}
+</template>
+
+<script lang="ts" src="./movieCard.ts"/>
+<style lang="sass" scoped src="./movieCard.sass"/>
