84a25215337f330a15dc86250f2089cbc2fe7adf
Daniel Quero <danielquero95@gmail.com>
2023-09-13 16:36:18 +0200

    Created getMovieDetail usecase and type, also added to container

--- src/application/types/MovieTypes.ts
+++ src/application/types/MovieTypes.ts
@@ -1,4 +1,5 @@
 export const MOVIE_TYPES = {
 	MOVIE_REPOSITORY: Symbol.for('MOVIE_REPOSITORY'),
 	GET_MOVIES_LIST: Symbol.for('GET_MOVIES_LIST'),
+	GET_MOVIE_DETAIL: Symbol.for('GET_MOVIE_DETAIL'),
 }
