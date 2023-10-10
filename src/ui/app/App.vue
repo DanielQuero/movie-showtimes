55c280b40322c56cc757f5e496bd5878c9cd3c9e
Daniel Quero <danielquero95@gmail.com>
2023-10-10 18:57:23 +0200

    Added footer and changed styles

--- src/ui/app/App.vue
+++ src/ui/app/App.vue
@@ -2,6 +2,14 @@
 TopBar
 main
   RouterView
+footer
+  .container.footer-container
+    .extra
+      span Made by&nbsp;
+      a(href="https://danielquero.github.io/" target="_blank") Daniel Quero
+    .attributions-box
+      img(src="tmdb.svg")
+      | This product uses the TMDB API but is not endorsed or certified by TMDB.
 </template>
 
 <script lang="ts">
@@ -14,10 +22,29 @@ import { Component, Vue } from 'vue-facing-decorator'
     TopBar,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  year = (new Date()).getFullYear()
+}
 </script>
 
 <style lang="sass">
 main
   padding-top: 60px
+footer
+  .footer-container
+    display: flex
+    align-items: center
+    justify-content: space-between
+    flex-wrap: wrap
+    .extra
+      font-size: 20px !important
+      flex: 1
+    .attributions-box
+      max-width: 320px
+      font-size: 12px !important
+      display: flex
+      img
+        width: 60px
+        margin-right: 10px
+
 </style>
